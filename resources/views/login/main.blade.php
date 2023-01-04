e2fc5f1a49fafd1ed2e56fa04b21d8ac5019e605
tecnoserviciosartavia <72755012+tecnoserviciosartavia@users.noreply.github.com>
2023-01-03 22:06:24 -0600

    strings

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -19,7 +19,7 @@
                 <div class="my-auto">
                     <img alt="TicoTiempos" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/logos/logos-new.png') }}">
                     <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">TicoTiempos</div>
-                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Para obtener una cuenta puedes comunicarte a travez del siguiente correo <br> con el administrador del sitio tiemposdarwins@gmail.com</div>
+                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Para obtener una cuenta puedes comunicarte con soporte@ticotiempos.com</div>
                 </div>
             </div>
             <!-- END: Login Info -->
